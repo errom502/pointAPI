a17e60978b6160c6aa9e5e50fb30a659b7e0aa1b
errom502 <79211993+errom502@users.noreply.github.com>
2022-08-29 13:05:34 +0300

    commit before pull

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	var c models.Client
 	c.Login = "Megat fj1df"
 	c.Password = "ssega 2"
-	//reg_result := client.Registration(c, db) //проверяет используется логин или нет
+	//reg_result := client.Registration(c, db) // + проверяет используется логин или нет
 	//fmt.Println(reg_result)
 	fmt.Println(client.Login(c, db))
 }
