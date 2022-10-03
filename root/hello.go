2de30a8221d948f580255e6ee6f9f411a2d67fea
errom502 <79211993+errom502@users.noreply.github.com>
2022-10-03 20:04:50 +0300

    hello fix

--- root/hello.go
+++ root/hello.go
@@ -5,8 +5,10 @@ import (
 	"encore.app/models"
 )
 
+//	msg := "Welcome to our API!\nHere's examples how you can use api:\n/client/reg\n{\n    \"login\": \"your_login\",\n    \"password\": \"password\"\n}\n/client/login\n{\n    \"login\": \"your_login\",\n    \"password\": \"password\"\n}\n/bookmark/add\n{\n    \"token\": \"your_token\",\n    \"name\": \"name of your bookmark\",\n    \"latitude\": 34.2242,\n    \"longitude\": 7.2325,\n    \"info\": \"some info of bookmark\"\n}\n/bookmarks/your_token\n\n/bookmark/edit\n{\n    \"id\": 0,\n    \"token\": \"your_token\",\n    \"name\": \"CHANGED NAME\",\n    \"latitude\": 34.2242,\n    \"longitude\": 7.2325,\n    \"info\": \"some info of bookmark\"\n}\n/client/delete\n{\n    \"token\": \"your token\"\n}\n/bookmark/delete\n{\n    \"token\": \"your_token\",\n    \"id\": 1\n}"
+//
 //encore:api public path=/hello
 func HelloWorld(ctx context.Context) (*models.Response, error) {
-	msg := "Welcome to our API!"
+	msg := "Welcome to our API!Here's examples how you can use api:\n/client/reg\n{\n    \"login\": \"your_login\",\n    \"password\": \"password\"\n}\n/client/login\n{\n    \"login\": \"your_login\",\n    \"password\": \"password\"\n}\n/bookmark/add\n{\n    \"token\": \"your_token\",\n    \"name\": \"name of your bookmark\",\n    \"latitude\": 34.2242,\n    \"longitude\": 7.2325,\n    \"info\": \"some info of bookmark\"\n}\n/bookmarks/your_token\n\n/bookmark/edit\n{\n    \"id\": 0,\n    \"token\": \"your_token\",\n    \"name\": \"CHANGED NAME\",\n    \"latitude\": 34.2242,\n    \"longitude\": 7.2325,\n    \"info\": \"some info of bookmark\"\n}\n/client/delete\n{\n    \"token\": \"your token\"\n}\n/bookmark/delete\n{\n    \"token\": \"your_token\",\n    \"id\": 1\n}"
 	return &models.Response{Message: msg}, nil
 }
